34cb18688ced6d24b8151321e7ce5f121e413c28
linsd <linsd>
2016-09-25 17:53:47 +0800

    md5

--- src/test/java/com/tasxxz/myapp/util/MD5UtilsTest.java
+++ src/test/java/com/tasxxz/myapp/util/MD5UtilsTest.java
@@ -4,6 +4,10 @@ import com.tasxxz.myapp.encrypt.MD5Utils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MD5UtilsTest {
 
 
@@ -12,6 +16,7 @@ public class MD5UtilsTest {
 
 		String input = "ksdif453士大夫";
 		System.out.println(MD5Utils.encode(input));
+		System.out.println(MD5Utils.encode(input.getBytes()));
 	}
 
 	@Test
@@ -19,4 +24,28 @@ public class MD5UtilsTest {
 		byte[] bytes = MD5Utils.digest("12w");
 		Assert.assertTrue(MD5Utils.byteArrayToHexString(bytes).equals(MD5Utils.byteArrayToHexString2(bytes)));
 	}
+
+	@Test
+	public void testValidFile() {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("D:/temp/HTTPClient-0.3-3.pom");
+			byte[] b = new byte[fis.available()];
+			fis.read(b);
+			System.out.println(MD5Utils.encode(b));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
